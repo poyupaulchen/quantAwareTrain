032e2e8ed502eb2a1e1843ff27f9d551c78ccc7e
poyupaulchen <poyupaulchen@gmail.com>
2024-08-13 20:13:31 -0700

    update

--- quantization_example.py
+++ quantization_example.py
@@ -7,7 +7,7 @@ quantize_scope = tfmot.quantization.keras.quantize_scope
 LastValueQuantizer = tfmot.quantization.keras.quantizers.LastValueQuantizer
 MovingAverageQuantizer = tfmot.quantization.keras.quantizers.MovingAverageQuantizer
 
-
+# config for quantizing the model
 class DefaultLinearQuantizeConfig(tfmot.quantization.keras.QuantizeConfig):
     # Configure how to quantize weights.
     def get_weights_and_quantizers(self, layer):
@@ -22,12 +22,12 @@ class DefaultLinearQuantizeConfig(tfmot.quantization.keras.QuantizeConfig):
         # Add this line for each item returned in `get_weights_and_quantizers`
         # , in the same order
         layer.dense.kernel = quantize_weights[0]
-        # return
+
     def set_quantize_activations(self, layer, quantize_activations):
         # Add this line for each item returned in `get_activations_and_quantizers`
         # , in the same order.
         layer.dense.activation = quantize_activations[0]
-        # return
+
     # Configure how to quantize outputs (may be equivalent to activations).
     def get_output_quantizers(self, layer):
         return []
@@ -35,6 +35,7 @@ class DefaultLinearQuantizeConfig(tfmot.quantization.keras.QuantizeConfig):
     def get_config(self):
         return {}
 
+# Define a custom layer
 @tf.keras.saving.register_keras_serializable()
 class Linear(tf.keras.layers.Layer):
     def __init__(self, units=32, name="linear", **kwargs):
@@ -52,12 +53,14 @@ class Linear(tf.keras.layers.Layer):
         """ Calls the layer """
         y = self.dense(inputs)
         return y
-class DefaultLinear1QuantizeConfig(DefaultLinearQuantizeConfig):
-    pass
-@tf.keras.saving.register_keras_serializable()
-class Linear1(Linear):
-    pass
+    
+# class DefaultLinear1QuantizeConfig(DefaultLinearQuantizeConfig):
+#     pass
+# @tf.keras.saving.register_keras_serializable()
+# class Linear1(Linear):
+#     pass
 
+# Define a custom model
 @tf.keras.saving.register_keras_serializable()
 class CustomModel(tf.keras.Model):
     def __init__(self, units, is_quant=False, name="CustomModel", **kwargs):
@@ -65,13 +68,14 @@ class CustomModel(tf.keras.Model):
         super(CustomModel, self).__init__(name=name, **kwargs)
         self.units = units
         if is_quant:
+            #quantize the layer
             self.layer1 = quantize_annotate_layer(Linear(3, name="Linear_1"), DefaultLinearQuantizeConfig())
             # self.layer1 = Linear(3)
             # self.layer2 = quantize_annotate_layer(Linear1(3, name='Linear_2'), DefaultLinear1QuantizeConfig())
         else:
             self.layer1 = Linear(3)
             # self.layer2 = tf.keras.layers.Dense(3)
-        
+
     def call(self, inputs):
         """ Calls the model """
         y = self.layer1(inputs)
@@ -88,51 +92,68 @@ class CustomModel(tf.keras.Model):
         )
         return config
 
-model = CustomModel(3)
-inputs = tf.keras.Input(shape=(3,))
-base_model = tf.keras.Model(inputs=inputs, outputs=model.call(inputs))
+if __name__ == "__main__":
+    # define the base model
+    model = CustomModel(3)
+    inputs = tf.keras.Input(shape=(3,))
+    base_model = tf.keras.Model(inputs=inputs, outputs=model.call(inputs))
 
-for v in base_model.trainable_variables:
-    v.assign(v*0+1)
+    # force the weights to be ones
+    for v in base_model.trainable_variables:
+        v.assign(v*0+1)
 
-model_quant = CustomModel(3, is_quant=True)
-base_model_quant = tf.keras.Model(inputs=inputs, outputs=model_quant.call(inputs))
+    # define the quantized model
+    model_quant = CustomModel(3, is_quant=True)
+    base_model_quant = tf.keras.Model(inputs=inputs, outputs=model_quant.call(inputs))
 
+    # copy the weights from the base model to the quantized model
+    for u,v in zip(base_model.trainable_variables, base_model_quant.trainable_variables):
+        v.assign(u)
 
-for u,v in zip(base_model.trainable_variables, base_model_quant.trainable_variables):
-    v.assign(u)
+    # apply quantization
+    with quantize_scope(
+            {   'DefaultLinearQuantizeConfig': DefaultLinearQuantizeConfig,
+                'Linear': Linear},
+            {   'DefaultLinear1QuantizeConfig': DefaultLinearQuantizeConfig,
+                'Linear': Linear1},
+        ):
+        quant_aware_model = tfmot.quantization.keras.quantize_apply(base_model_quant)
+    quant_aware_model.summary()
 
-with quantize_scope(
-        {   'DefaultLinearQuantizeConfig': DefaultLinearQuantizeConfig,
-            'Linear': Linear},
-        {   'DefaultLinear1QuantizeConfig': DefaultLinearQuantizeConfig,
-            'Linear': Linear1},
-    ):
-    quant_aware_model = tfmot.quantization.keras.quantize_apply(base_model_quant)
-# quant_aware_model.summary()
-# for v in base_model.trainable_variables:
-#     print(v)
+    # print the variables of the quantized model
+    for v in quant_aware_model.variables:
+        print(v.name)
+        print(v)
+        print("")
 
-for v in quant_aware_model.variables:
-    print(v.name)
-    print(v)
-    print("")
-# quant_aware_model.variables[0].assign(-1)
-# quant_aware_model.variables[1].assign(1)
-# W= [[1,1,1],
-#     [1,1,1],
-#     [1,1,1]]
-# b= [[1],
-#     [1],
-#     [1]]
-# x= [[1],
-#     [2],
-#     [3]]
-# y = W*x + b
+    # set the input min and max for the quantized model
+    
+    # W= [[1,1,1],
+    #     [1,1,1],
+    #     [1,1,1]]
+    # b= [[1],
+    #     [1],
+    #     [1]]
+    # x= [[1],
+    #     [2],
+    #     [3]]
+    # y = W*x + b
 
-x = np.array([[1,2,3]], dtype=np.float32)
-out = base_model(x)
-print(out)
-out = quant_aware_model(x)
-print(out)
-import pdb; pdb.set_trace()
+    # test the base model
+    x = np.array([[1,2,3]], dtype=np.float32)
+    out = base_model(x)
+    print("Base model output:")
+    print(out)
+
+    # test the quantized model before setting the input min and max
+    out = quant_aware_model(x)
+    print("Quantized model output:")
+    print(out)
+
+    quant_aware_model.variables[0].assign(-1)
+    quant_aware_model.variables[1].assign(1)
+
+    # test the quantized model after setting the input min and max
+    out = quant_aware_model(x)
+    print("Quantized model output:")
+    print(out)
